31f08b03afc66e8381c7b2f66d1b46ddfcd164ec
luc0liv <luoliveiradacunha@gmail.com>
2023-09-04 12:34:48 -0300

    feat: cria classe Livro e implementa métodos emprestar e devolver

---
+++ ExercicioBiblioteca/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBiblioteca
+{
+    internal class Livro : ItemBiblioteca, IPodeSerEmprestado
+    {
+        public DateTime DataDeEmprestimo { get; set; }
+        public DateTime DataDeDevolucao { get; set; }
+        public Livro(string titulo, string autor, int anoDePublicacao, int numeroDePaginas)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnoDePublicacao = anoDePublicacao;
+            NumeroDePaginas = numeroDePaginas;
+        }
+
+        public void Emprestar()
+        {
+            DataDeEmprestimo = DateTime.Now;
+            DataDeDevolucao = new DateTime(DataDeEmprestimo.Year, DataDeEmprestimo.Month, DataDeEmprestimo.Day + 7);
+            Console.WriteLine($"Empréstimo do livro {Titulo} realizado no dia {DataDeEmprestimo.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Deverá ser devolvido na data: {DataDeDevolucao.ToString("dd/MM/yyyy")}");
+        }
+
+        public void Devolver(DateTime devolucao)
+        {
+            if (devolucao > DataDeDevolucao)
+            {
+                Console.WriteLine($"Entrega do livro {Titulo} com atraso");
+            } else
+            {
+                Console.WriteLine($"Devolução do livro {Titulo} realizada");
+            }
+
+        }
+    }
+}
