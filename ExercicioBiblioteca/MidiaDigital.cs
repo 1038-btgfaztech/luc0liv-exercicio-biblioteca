76f75b2bfe844e55f3e8ac710b085954c3d6b170
luc0liv <luoliveiradacunha@gmail.com>
2023-09-04 15:45:41 -0300

    feat: cria classe Midia Digital

---
+++ ExercicioBiblioteca/MidiaDigital.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBiblioteca
+{
+    internal class MidiaDigital : ItemBiblioteca, IPodeSerEmprestado
+    {
+        public MidiaDigital(string titulo, string autor, int anoDePublicacao, int numeroDePaginas)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnoDePublicacao = anoDePublicacao;
+            NumeroDePaginas = numeroDePaginas;
+        }
+
+        public void Emprestar()
+        {
+            Console.WriteLine($"Mídia digital {Titulo} emprestada na data {DateTime.Now.ToString("dd/MM/yyyy")}");
+        }
+
+        public void Devolver(DateTime devolucao)
+        {
+            Console.WriteLine($"Mídia Digital {Titulo} devolvida na data {devolucao.ToString("dd/MM/yyyy")}");
+        }
+    }
+}
