1ba13a5e5c5fcd4b6d428e6101024efe6871b118
luc0liv <luoliveiradacunha@gmail.com>
2023-09-04 10:56:14 -0300

    Commit inicial

---
+++ ExercicioBiblioteca/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
