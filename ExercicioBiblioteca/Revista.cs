a16b56ed00ae80db3e64c2940bee9676c360e281
luc0liv <luoliveiradacunha@gmail.com>
2023-09-04 15:45:53 -0300

    feat: cria classe Revista

---
+++ ExercicioBiblioteca/Revista.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBiblioteca
+{
+    internal class Revista : ItemBiblioteca
+    {
+        public Revista(string titulo, string autor, int anoDePublicacao, int numeroDePaginas)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnoDePublicacao = anoDePublicacao;
+            NumeroDePaginas = numeroDePaginas;
+        }
+    }
+}
