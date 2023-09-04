11399725cc0b83b35b56697e99a48a5214fec8de
luc0liv <luoliveiradacunha@gmail.com>
2023-09-04 12:34:10 -0300

    feat: cria classes ItemBiblioteca e interface IPodeSerEmprestado

---
+++ ExercicioBiblioteca/ItemBiblioteca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBiblioteca
+{
+    internal abstract class ItemBiblioteca
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoDePublicacao { get; set; }
+        public int NumeroDePaginas { get; set; }
+    }
+}
